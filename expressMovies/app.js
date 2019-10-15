c55a1b1a6275624e727979e15661f8fe154e96df
Rémy Jacob <rjpro77@outlook.fr>
2019-10-15 22:10:30 +0200

    added function stock token in local storage and added function who change the navbar when the user is locked

--- expressMovies/app.js
+++ expressMovies/app.js
@@ -91,7 +91,7 @@ app.get('/movie-search', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-  res.render('login', { title: 'connexion' })
+  res.render('login', { title: 'Espace menbre' })
 })
 
 const fakeUser = { email: 'test@testmail.fr', password: 'azerty' }
